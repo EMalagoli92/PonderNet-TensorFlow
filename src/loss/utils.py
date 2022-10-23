d01f2e41de653e2ba1c73b2b75de494feeecb309
EMalagoli92 <emala.892@gmail.com>
2022-10-23 10:35:17 +0200

    Update utils.py

--- src/loss/utils.py
+++ src/loss/utils.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 
 
 class KLDiv(tf.keras.losses.Loss):
+    '''
+    The Kullback-Leibler divergence loss with 'batchmean' reduction.
+    '''
     def __call__(self,y_true: tf.Tensor,y_pred: tf.Tensor) -> tf.Tensor:
         batch = y_true.get_shape()[0]
         return tf.math.reduce_sum(y_pred * (tf.math.log(y_pred) - y_true)) / batch
