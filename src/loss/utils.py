00120cde02fa8866986446c971872e28b60f85ba
EMalagoli92 <emala.892@gmail.com>
2022-10-23 18:04:02 +0200

    Update utils.py

--- src/loss/utils.py
+++ src/loss/utils.py
@@ -10,5 +10,5 @@ class KLDiv(tf.keras.losses.Loss):
                  y_true: tf.Tensor,
                  y_pred: tf.Tensor
                  ) -> tf.Tensor:
-        batch = tf.shape(y_true,out_type = tf.keras.backend.floatx())[0]
-        return tf.math.reduce_sum(y_pred * (tf.math.log(y_pred) - y_true)) / batch
+        batch_size = tf.shape(y_true,out_type = tf.keras.backend.floatx())[0]
+        return tf.math.reduce_sum(y_pred * (tf.math.log(y_pred) - y_true)) / batch_size
